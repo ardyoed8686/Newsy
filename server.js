c8130fefb0935ab1f750c97d0654467a2378257c
Romy Owens <rdoed160@gmail.com>
2019-11-17 22:35:09 -0500

    added models Article, Note & index

--- server.js
+++ server.js
@@ -12,8 +12,35 @@ var cheerio = require("cheerio");
 // 5. axios
 var axios = require("axios");
 
+
+// Mongoose
+
+var Note = require("./models/Note");
+var Article = require("./models/Article");
+var databaseUrl = 'mongodb://localhost/nyt';
+
+if (process.env.MONGODB_URI) {
+	mongoose.connect(process.env.MONGODB_URI);
+}
+else {
+	mongoose.connect(databaseUrl);
+};
+
+mongoose.Promise = Promise;
+var db = mongoose.connection;
+
+db.on("error", function(error) {
+	console.log("Mongoose Error: ", error);
+});
+
+db.once("open", function() {
+	console.log("Mongoose connection successful.");
+});
+
 // Initialize Express
 var app = express();
+
+var PORT = process.env.PORT || 3000;
 
 // Configure middleware
 // Use morgan logger for logging requests
@@ -22,21 +49,27 @@ app.use(logger("dev"));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 // Make public a static folder
-app.use(express.static("public"));
+app.use(express.static(__dirname + "/public"));
 
-// If deployed, use the deployed database. Otherwise use the local mongoHeadlines database
-var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/mongoHeadlines";
+app.engine("handlebars", exphbs({defaultLayout: "main"}));
+app.set("view engine", "handlebars");
 
-mongoose.connect(MONGODB_URI);
+
+
+
+app.listen(PORT, function() {
+ console.log("Listening on port:" + PORT);
+});
+
 
 // Routes
 
 // Make a request via axios to grab the HTML body from the site of your choice
-axios.get("https://www..com").then(function(response) {
+// axios.get("https://www..com").then(function(response) {
 
   // Load the HTML into cheerio and save it to a variable
   // '$' becomes a shorthand for cheerio's selector commands, much like jQuery's '$'
-  var $ = cheerio.load(response.data);
-console.log(response.data)
+//   var $ = cheerio.load(response.data);
+// console.log(response.data)
   // An empty array to save the data that we'll scrape
-  var results = [];
+  // var results = [];
