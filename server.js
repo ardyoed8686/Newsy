a7dbea3051cbe843b1fcef1a4389379e6678cbda
Romy Owens <rdoed160@gmail.com>
2019-11-17 23:33:30 -0500

    added routes to find, post, save and scrape

--- server.js
+++ server.js
@@ -1,15 +1,9 @@
 
-// Run `npm init`. When that's finished, install and save these npm packages:
 var logger = require("morgan");
-// 1. express
 var express = require("express");
-// 2. express-handlebars
 var express = require("express-handlebars");
-// 3. mongoose
 var mongoose = require("mongoose");
-// 4. cheerio
 var cheerio = require("cheerio");
-// 5. axios
 var axios = require("axios");
 
 
@@ -64,12 +58,79 @@ app.listen(PORT, function() {
 
 // Routes
 
-// Make a request via axios to grab the HTML body from the site of your choice
-// axios.get("https://www..com").then(function(response) {
+app.get("/", function(req, res) {
+	Article.find({}, null, {sort: {created: -1}}, function(err, data) {
+		if(data.length === 0) {
+			res.render("placeholder", {message: "Nothing scraped yet. Please click \"Scrape New Articles\" for new listings."});
+		}
+		else{
+			res.render("index", {articles: data});
+		}
+	});
+});
 
-  // Load the HTML into cheerio and save it to a variable
-  // '$' becomes a shorthand for cheerio's selector commands, much like jQuery's '$'
-//   var $ = cheerio.load(response.data);
-// console.log(response.data)
-  // An empty array to save the data that we'll scrape
-  // var results = [];
+app.get("/scrape", function(req, res) {
+  // First, we grab the body of the html with axios
+  axios.get("https://www.nytimes.com/section/world").then(function(response) {
+    var $ = cheerio.load(response.data);
+    $("div.story-body").each(function(i, element) {
+     
+      var result = {};
+      result.title = $(this).find("h2.headline").text().trim();
+      result.link = $(this).find("a").attr("href");
+      result.summary = $(element).find("p.summary").text().trim();
+
+      // Create a new Article using the `result` object built from scraping
+      db.Article.create(result)
+        .then(function(dbArticle) {
+          // View the added result in the console
+          console.log(dbArticle);
+        })
+        .catch(function(err) {
+          // If an error occurred, log it
+          console.log(err);
+        });
+    });
+
+    // Send a message to the client
+    res.send("Scrape Complete");
+    res.redirect("/");
+  });
+});
+
+app.get("/saved", function(req, res) {
+	Article.find({issaved: true}, null, {sort: {created: -1}}, function(err, data) {
+		if(data.length === 0) {
+			res.render("placeholder", {message: "You have not saved any articles yet. Try saving some articles by clicking \"Save Article\"!"});
+		}
+		else {
+			res.render("saved", {saved: data});
+		}
+	});
+});
+
+app.get("/:id", function(req, res) {
+	Article.findById(req.params.id, function(err, data) {
+		res.json(data);
+	})
+});
+
+app.post("/note/:id", function(req, res) {
+	var note = new Note(req.body);
+	note.save(function(err, doc) {
+		if (err) throw err;
+		Article.findByIdAndUpdate(req.params.id, {$set: {"note": doc._id}}, {new: true}, function(err, newdoc) {
+			if (err) throw err;
+			else {
+				res.send(newdoc);
+			}
+		});
+	});
+});
+
+app.get("/note/:id", function(req, res) {
+	var id = req.params.id;
+	Article.findById(id).populate("note").exec(function(err, data) {
+		res.send(data.note);
+	})
+})
